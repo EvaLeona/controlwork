3b3023c2796d8eed822dc42bcc0a52f74f77e382
Eva <eva.leona@mail.ru>
2022-12-09 12:09:01 +0300

    цикл для заполнения массива строками

--- control_work.cs
+++ control_work.cs
@@ -1,2 +1,13 @@
 ﻿using System;
 
+public class MainClass
+{
+    public static void Main()
+    {
+        string[] arrays = new string[5]; 
+
+        for (int i = 0; i < arrays.Length; i++) 
+        {
+            Console.WriteLine("Введите что угодно:");
+            arrays[i] = Console.ReadLine();
+        }
