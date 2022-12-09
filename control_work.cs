372b3f397a7325fbebc1963140f85d86d2755e4f
Eva <eva.leona@mail.ru>
2022-12-09 12:15:34 +0300

    //Цикл, заполняющий массив строками, длина которых меньше либо равна 3

--- control_work.cs
+++ control_work.cs
@@ -20,6 +20,19 @@ public class MainClass
                 a++;
             }
         }
-        string[] newarray = new string[a]; 
+        string[] newarray = new string[a];
         a = 0;
+        for (int i = 0; i < arrays.Length; i++) 
+        {
+            if (arrays[i].Length <= 3)
+            {
+                newarray[a] = arrays[i];
+                a++;
+                Console.WriteLine("В массиве: " + arrays[a]);
+            }
+        }
+
+    }
+}
+        
        
